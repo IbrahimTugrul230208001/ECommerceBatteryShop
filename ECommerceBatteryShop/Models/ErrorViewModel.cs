89ead989f19d7aeb7c34f2423148cdb8e2d1a177
IbrahimTugrul230208001 <230208001@ostimteknik.edu.tr>
2025-07-25 16:04:03 +0300

    Add project files.

---
+++ ECommerceBatteryShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ECommerceBatteryShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
