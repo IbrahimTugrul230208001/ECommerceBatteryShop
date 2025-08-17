1d786b41518277d57ef1e6088d74a1141f3df58c
IbrahimTugrul230208001 <230208001@ostimteknik.edu.tr>
2025-08-17 11:49:53 +0300

    .

---
+++ ECommerceBatteryShop.DataAccess/BatteryShopContext.cs
@@ -0,0 +1,33 @@
+﻿using ECommerceBatteryShop.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceBatteryShop.DataAccess
+{
+    public class BatteryShopCOntext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=ibrahim06;Database=ecommercebatteryshop;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            const string trCollation = "tr_icu_det";   // PostgreSQL deterministic ICU collation
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(b => b.Name)
+                      .UseCollation(trCollation);
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
--- ECommerceBatteryShop.DataAccess/Entities/Address.cs
+++ ECommerceBatteryShop.DataAccess/Entities/Address.cs
@@ -6,7 +6,4 @@ public class Address
     public int UserId { get; set; }
     public string Street { get; set; } = string.Empty;
     public string City { get; set; } = string.Empty;
-    public string State { get; set; } = string.Empty;
-    public string PostalCode { get; set; } = string.Empty;
-    public string Country { get; set; } = string.Empty;
 }
