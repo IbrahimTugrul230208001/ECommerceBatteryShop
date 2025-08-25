d43190950a168b2ef83f2ca8accefb8fb138c9ec
IbrahimTugrul230208001 <230208001@ostimteknik.edu.tr>
2025-08-25 08:50:56 +0300

    Backend configuration

---
+++ ECommerceBatteryShop/Models/PriceViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerceBatteryShop.Models
+{
+    public sealed class PriceViewModel
+    {
+        public decimal UsdTry { get; set; }
+        public string? UsdTryFormatted { get; set; }
+    }
+}
