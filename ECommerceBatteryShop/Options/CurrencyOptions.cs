13db55b35e0413448d29936b952cf01c5d64f481
IbrahimTugrul230208001 <230208001@ostimteknik.edu.tr>
2025-08-26 10:15:01 +0300

    .

--- ECommerceBatteryShop/Options/CurrencyOptions.cs
+++ ECommerceBatteryShop/Options/CurrencyOptions.cs
@@ -2,7 +2,7 @@
 {
     public sealed class CurrencyOptions
     {
-        public string BaseUrl { get; set; } = "https://api.exchangerate.host/";
+        public string BaseUrl { get; set; } = "https://open.er-api.com/v6/";
         public int CacheSeconds { get; set; } = 600; // 10 min
     }
 }
