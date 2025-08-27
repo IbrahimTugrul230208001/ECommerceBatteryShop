444fec93a2df413bf6b858afdca8dc95af26bf20
IbrahimTugrul230208001 <230208001@ostimteknik.edu.tr>
2025-08-27 12:22:45 +0300

    account

--- ECommerceBatteryShop/Controllers/AccountController.cs
+++ ECommerceBatteryShop/Controllers/AccountController.cs
@@ -4,7 +4,27 @@ namespace ECommerceBatteryShop.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+        public IActionResult VerifyAccount()
         {
             return View();
         }
