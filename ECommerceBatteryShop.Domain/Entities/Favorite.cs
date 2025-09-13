e66f86f1507cbba6b67c01e38c7dce25f51c16e6
IbrahimTugrul230208001 <230208001@ostimteknik.edu.tr>
2025-09-13 20:28:03 +0300

    .

---
+++ ECommerceBatteryShop.Domain/Entities/Favorite.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceBatteryShop.Domain.Entities
+{
+    public class Favorite
+    {
+        public int Id { get; set; }
+
+        // either UserId (for logged-in) OR AnonId (for guest) will be set
+        public int? UserId { get; set; }
+        public User? User { get; set; }
+
+        public string? AnonId { get; set; }   // <-- add this
+        public DateTime CreatedAt { get; set; }
+
+        public ICollection<CartItem> Items { get; set; } = new List<CartItem>();
+    }
+}
