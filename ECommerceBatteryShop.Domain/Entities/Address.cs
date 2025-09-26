793844d397a79892607502d6977761f14aa6eabe
IbrahimTugrul230208001 <230208001@ostimteknik.edu.tr>
2025-09-26 08:28:27 +0300

    addresses changes

--- ECommerceBatteryShop.Domain/Entities/Address.cs
+++ ECommerceBatteryShop.Domain/Entities/Address.cs
@@ -5,8 +5,14 @@ public class Address
     public int Id { get; set; }
     public int UserId { get; set; }
     public User? User { get; set; }
-    public string Street { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Surname { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string FullAddress { get; set; } = string.Empty;
     public string City { get; set; } = string.Empty;
-    public string? State { get; set; }
+    public string State { get; set; } = string.Empty;
+    public string NeightBourhood { get; set; } = string.Empty;
+
     public ICollection<Order> Orders { get; set; } = new List<Order>();
 }
