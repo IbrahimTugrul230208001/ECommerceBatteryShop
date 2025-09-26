cc0760ed247e2b26db787d69c94261ef12bc2c80
IbrahimTugrul230208001 <230208001@ostimteknik.edu.tr>
2025-09-26 09:06:22 +0300

    new changes on order

---
+++ ECommerceBatteryShop.DataAccess/Concrete/OrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using ECommerceBatteryShop.DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceBatteryShop.DataAccess.Concrete
+{
+    public class OrderRepository : IOrderRepository
+    {
+
+    }
+}
--- ECommerceBatteryShop/Controllers/OrdersController.cs
+++ ECommerceBatteryShop/Controllers/OrdersController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerceBatteryShop.Models;
+using ECommerceBatteryShop.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceBatteryShop.Controllers
 {
     public class OrdersController : Controller
     {
+        public readonly ICartService _cartService;
         public IActionResult Index()
         {
             return View();
+        }
+        public async Task<IActionResult> InsertOrder()
+        {
+            CartOwner owner;
+            var userId = int.Parse(User.FindFirst("sub")!.Value);
+            owner = CartOwner.FromUser(userId);
+            
+            var cart = await _cartService.GetAsync(owner);
+            if (cart is null || !cart.Items.Any())
+                return RedirectToAction("Index", "Cart");
+            return View(cart);
         }
     }
 }
