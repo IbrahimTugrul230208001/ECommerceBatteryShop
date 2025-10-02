0e0e8c0c9e34612a3443800a8e720a5e79e2e0e6
IbrahimTugrul230208001 <230208001@ostimteknik.edu.tr>
2025-10-02 09:33:26 +0300

    .

--- ECommerceBatteryShop.DataAccess/Abstract/IOrderRepository.cs
+++ ECommerceBatteryShop.DataAccess/Abstract/IOrderRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ECommerceBatteryShop.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace ECommerceBatteryShop.DataAccess.Abstract
 {
     public interface IOrderRepository
     {
+        Task<Order> InsertOrderAsync(Order order, CancellationToken ct=default);
     }
 }
--- ECommerceBatteryShop.DataAccess/Concrete/OrderRepository.cs
+++ ECommerceBatteryShop.DataAccess/Concrete/OrderRepository.cs
@@ -1,4 +1,6 @@
 ﻿using ECommerceBatteryShop.DataAccess.Abstract;
+using ECommerceBatteryShop.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,26 @@ namespace ECommerceBatteryShop.DataAccess.Concrete
 {
     public class OrderRepository : IOrderRepository
     {
+        private readonly BatteryShopContext _ctx;
 
+        public OrderRepository(BatteryShopContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public async Task<Order> InsertOrderAsync(Order order, CancellationToken ct = default)
+        {
+            await _ctx.Orders.AddAsync(order, ct);
+            await _ctx.SaveChangesAsync(ct);
+            return order; // now has generated PK
+        }
+        public async Task CancelOrder(int orderId)
+        {
+            var order = await _ctx.Orders.Where(o => o.OrderId == orderId).FirstOrDefaultAsync();
+            if (order != null)
+            {
+                order.Status = "İptal edildi";
+                await _ctx.SaveChangesAsync();
+            }
+        }
     }
 }
