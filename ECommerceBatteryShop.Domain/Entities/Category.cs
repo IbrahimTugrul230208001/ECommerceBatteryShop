ad820df644d2d13d6a59d8818fa58ad994c6ea60
IbrahimTugrul230208001 <230208001@ostimteknik.edu.tr>
2025-10-03 11:30:33 +0300

    checkpoint - 03.10.2025

--- ECommerceBatteryShop.Domain/Entities/Category.cs
+++ ECommerceBatteryShop.Domain/Entities/Category.cs
@@ -4,7 +4,6 @@ public class Category
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public string? Description { get; set; }
     public int? ParentCategoryId { get; set; }
     public Category? ParentCategory { get; set; }
     public string Depth { get; set; } = string.Empty;
