73e38b0a613a9b8cb6a1bf4b2ab38a472353dc3d
IbrahimTugrul230208001 <230208001@ostimteknik.edu.tr>
2026-05-05 23:46:13 +0300

    new domain and new updates.

--- ECommerceBatteryShop/Controllers/EvController.cs
+++ ECommerceBatteryShop/Controllers/EvController.cs
@@ -54,21 +54,13 @@ namespace ECommerceBatteryShop.Controllers
             // Ensure this includes ProductCategories (CategoryId is enough; Category.Include not required)
             var plan = new[]
             {
+                new { Title = "Lityum 12V Batarya Paketleri", CatId = batteryPackages12vId, CatSlug = "lityum-batarya-paketleri-12v" },
+                new { Title = "Lityum 24V Batarya Paketleri", CatId = batteryPackages24vId, CatSlug = "lityum-batarya-paketleri-24v" },
                 new { Title = "Lithium Polymer Pil", CatId = LiPolymerId, CatSlug = "lithium-polymer-pil" },
                 new { Title = "Punta Cihazları", CatId = puntaCihazıId, CatSlug = "punta-cihazi" },
                 new { Title = "Lithium-ion Pil", CatId = LiIonId, CatSlug = "lithium-ion-pil" },
                 new { Title = "BMS - Pil Koruma Devresi", CatId = BmsId, CatSlug = "bms-pil-koruma-devresi" },
                 new { Title = "LiFePO4 Pil", CatId = LfpId, CatSlug = "lifepo4-pil" },
-                new
-                {
-                    Title = "LifePO4 12V Batarya Paketleri", CatId = batteryPackages12vId,
-                    CatSlug = "lifepo4-batarya-paketleri-12v"
-                },
-                new
-                {
-                    Title = "LifePO4 24V Batarya Paketleri", CatId = batteryPackages24vId,
-                    CatSlug = "lifepo4-batarya-paketleri-24v"
-                },
                 new { Title = "Soketler", CatId = socketsId, CatSlug = "soketler" },
                 new { Title = "Silikon Kablolar", CatId = siliconCablesId, CatSlug = "silikon-kablolar" },
                 new { Title = "Bantlar", CatId = bandsId, CatSlug = "bantlar" }
